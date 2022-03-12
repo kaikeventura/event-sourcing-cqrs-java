12685477f4069a2b6529dbd828a7eea09e1cf4ef
kaikeventura <kaike.gnr@hotmail.com>
2022-03-11 23:06:25 -0300

    Construção da base do ms

---
+++ common/src/test/java/com/kaikeventura/wallet/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaikeventura.wallet.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
